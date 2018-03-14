aca35cda0bb732b800991835c7cb68e018dda841
SmokeySmoke <35714295+SmokeySmoke@users.noreply.github.com>
2018-03-13 22:25:24 -0400

    add chapter 11 exercise 12

---
+++ Chapter 11/Ch11ProgEX12.c
@@ -0,0 +1,38 @@
+/* Write a program that reads input up to EOF and reports the number of words,
+ * the number of uppercase letters, the number of lowercase letters, the number
+ * of punctuation characters, and the number of digits. Use the ctype.h family
+ * of functions.
+ */
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+int main(void)
+{
+	char input;
+	bool inword = false;
+	int nw, nup, nlow, npunc, ndig;
+	nw = nup = nlow = npunc = ndig = 0;
+
+	while ((input = getchar()) != EOF)
+	{
+		if (inword = true && isspace(input) && !ispunct(input))
+			nw++;
+		if (isupper(input))
+			nup++;
+		else if (islower(input))
+			nlow++;
+		else if (ispunct(input))
+			npunc++;
+		else if (isdigit(input))
+			ndig++;
+	}
+	printf("# words = 	%d\n"
+	       "# uppercase: 	%d\n"
+	       "# lowercase:	%d\n"
+	       "# punctuation:	%d\n"
+	       "# digits:	%d\n",
+	       nw, nup, nlow, npunc, ndig);
+
+	return 0;
+}
